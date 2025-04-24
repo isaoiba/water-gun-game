a98daf6571aa690a1eda011791be899aecfa56dc
levipass <100034191@mvla.net>
2025-04-24 13:53:24 -0700

    commit

--- prototype/Assets/Scripts/Player.cs
+++ prototype/Assets/Scripts/Player.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI countText;
+
+    // UI object to display winning text.
+    public GameObject playerob;
+    public GameObject winTextObject;
+    public int count;
 
     // Update is called once per frame
+    private void Start()
+    {
+        SetCountText();
+    }
+
     void Update()
     {
         
+    }
+    
+    public void SetCountText() 
+    {
+        // Update the count text with the current count.
+        countText.text = "Enemies killed: " + count + "/5";
+
+        // Check if the count has reached or exceeded the win condition.
+        if (count >= 5)
+        {
+            // Display the win text.
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
     }
 }
