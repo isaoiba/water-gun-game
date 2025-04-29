8271b5729a7865dd2950aea5831d65cf1d1e58bf
levipass <100034191@mvla.net>
2025-04-29 11:05:53 -0700

    urp/commit

--- prototype/Assets/Scripts/EnemyHandler.cs
+++ prototype/Assets/Scripts/EnemyHandler.cs
@@ -6,10 +6,21 @@ using UnityEngine;
 public class EnemyHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    
+    Rect rect = new Rect(0, 0, 300, 100);
+    Vector3 offset = new Vector3(0f, 0f, 0.5f); // height above the target position
+    
     public Player something;
 
-    public static float Health = 100f;
+    public float Health = 100f;
+
+    void OnGUI()
+    {
+        Vector3 point = Camera.main.WorldToScreenPoint(gameObject.transform.position + offset);
+        rect.x = point.x;
+        rect.y = Screen.height - point.y - rect.height; // bottom left corner set to the 3D point
+        GUI.Label(rect, "Health:"+ Health); // display its name, or other string
+    }
 
     private void Start()
     {
